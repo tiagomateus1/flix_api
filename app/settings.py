6cc274f6a38089fd50faed3603da3a5ae5485b88
tiagomateus1 <tiaguinho.mtt01@gmail.com>
2024-07-30 21:12:21 -0300

    First commit

--- app/settings.py
+++ app/settings.py
@@ -143,6 +143,6 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
 }
